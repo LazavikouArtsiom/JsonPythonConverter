0836edc750867450e884774a6b7b3c214a08f943
LazavikouArtsiom <lazovikovartyom@mail.ru>
2020-01-27 14:09:32 +0300

    CSV to Json done

--- jsonconverter.py
+++ jsonconverter.py
@@ -19,13 +19,14 @@ class CsvToJson(Converter):
             print("File is empty, choose another one")
             self.__init__()
         self.jsonfile = input("Enter the name of .json file : ")
+        self.jsonstring = ''
         
 
     def _get_header(self):
         self._keys = []
         try:
             with open(self.csvfile, 'r') as f:
-                self._keys = f.readline().split(sep=',')
+                self._keys = f.readline().rstrip().split(sep=',')
                 return self._keys
         except FileNotFoundError:
             print("The file doesn't exist")
@@ -37,16 +38,22 @@ class CsvToJson(Converter):
         try:
             with open(self.csvfile, 'r') as f:
                 for line in f:
+                    line = line.rstrip()
                     if self._i == 0:
                         self._i += 1
                         continue
-                    for i, item in enumerate(line.split(",")):
-                        line_to_add = ""
-                        if not item.isdigit():
-                            line_to_add += '"' + self._header[i] + '"' + ":" + '"' + item + '"'
+                    line_to_add = "{\n"
+                    _temp = line.split(",") #List of csv line elements
+                    for i, item in enumerate(_temp, 0):
+
+                        if not item.strip().isdigit():
+                                line_to_add += '\t"' + self._header[i] + '":"' + item + '",\n'
+                        elif not item:
+                                line_to_add += '\t"' + self._header[i] + '":" ",\n'
                         else:
-                            line_to_add += '"' + self._header[i] + '"' + ":" + item
-                        self.list_of_strings_to_add.append(line_to_add)
+                                line_to_add += '\t"' + self._header[i] + '":' + item + ",\n"
+                    line_to_add += "\t},\n"        
+                    self.list_of_strings_to_add.append(line_to_add)
                 return self.list_of_strings_to_add
         except FileNotFoundError:
             print("The file doesn't exist")
@@ -55,11 +62,11 @@ class CsvToJson(Converter):
     def parse(self):
         try:
             with open(self.jsonfile, 'x') as f:
-                self.jsonstring += '\n + "data":['
+                self.jsonstring += '{\n'
                 for line in self._get_values():
-                    self.jsonstring += "{" + line + "},\n"
-                self.jsonstring.rstip(",\n")
-                self.jsonstring += "]\n"
+                    self.jsonstring += "\t" + line
+                self.jsonstring.rstrip(",\n")
+                self.jsonstring += "}\n"
                 f.write(self.jsonstring)
                 return True
         except FileExistsError:
@@ -69,4 +76,6 @@ class CsvToJson(Converter):
 
 if __name__ == "__main__":
     k = CsvToJson()
+    k._get_header()
     k.parse()
+    
