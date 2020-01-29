0023436ad6deb59b99d2a333935db9094fdf3984
LazavikouArtsiom <lazovikovartyom@mail.ru>
2020-01-29 15:05:09 +0300

    FileManager added, file validations added

--- jsonconverter.py
+++ jsonconverter.py
@@ -1,72 +1,89 @@
 import os
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 
 
-class Converter:
+class FileManager:
+    def __init__(self, filename, access_mode):
+        self.filename = filename
+        self.access_mode = access_mode
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.filename, self.access_mode)
+        return self.file
+
+    def __exit__(self, *args, **kwargs):
+        self.file.close()
+
+
+class Converter(ABC):
     @abstractmethod
     def parse(self):
         pass
 
 
 class CsvToJson(Converter):
-    # use RE here
     def __init__(self):
         self.csvfile = input("Enter the name of .csv file : ")
-        if not os.path.exists(self.csvfile):
-            print("File doesn't exist")
-            self.__init__()
-        if os.stat(self.csvfile).st_size == 0:
+        while not os.path.exists(self.csvfile):
+            self.csvfile = input("Enter the name of .csv file : ")
+        while os.stat(self.csvfile).st_size == 0:
             print("File is empty, choose another one")
-            self.__init__()
-        self.jsonfile = input("Enter the name of .json file : ")
-        self.jsonstring = ''
-        
+            self.csvfile = input("Enter the name of .csv file : ")
 
-    def _get_header(self):
-        self._keys = []
-        try:
-            with open(self.csvfile, 'r') as f:
-                self._keys = f.readline().rstrip().split(sep=',')
-                return self._keys
-        except FileNotFoundError:
-            print("The file doesn't exist")
+        self.jsonfile = input("Enter the name of .json file : ")
+        while not self.jsonfile.endswith(".json"):
+            self.jsonfile = input(
+                "The name must be end with .json, try again : ")
+        self.jsonstring = ''
+
+    def _get_headers(self):
+        '''
+        Takes the very first line of .csv doc and split it up to take
+        all headers.
+        '''
+        self.list_of_headers = []
+        with FileManager(self.csvfile, 'r') as f:
+            self._keys = f.readline().rstrip().split(sep=',')
+            return self.list_of_headers
 
     def _get_values(self):
-        self._header = self._get_header()
+        self._headers = self._get_headers()
         self._i = 0
         self.list_of_strings_to_add = []
-        try:
-            with open(self.csvfile, 'r') as f:
-                for line in f:
-                    line = line.rstrip()
-                    if self._i == 0:
-                        self._i += 1
-                        continue
-                    line_to_add = "{\n"
-                    _temp = line.split(",") #List of csv line elements
-                    for i, item in enumerate(_temp, 0):
 
-                        if not item.strip().isdigit():
-                                line_to_add += '\t"' + self._header[i] + '":"' + item + '",\n'
-                        elif not item:
-                                line_to_add += '\t"' + self._header[i] + '":" ",\n'
-                        else:
-                                line_to_add += '\t"' + self._header[i] + '":' + item + ",\n"
-                    line_to_add += "\t},\n"        
-                    self.list_of_strings_to_add.append(line_to_add)
-                return self.list_of_strings_to_add
-        except FileNotFoundError:
-            print("The file doesn't exist")
+        with FileManager(self.csvfile, 'r') as f:
+            for line in f:
+                line = line.rstrip()
+                if self._i == 0:
+                    self._i += 1
+                    continue
+                line_to_add = "{\n"
+                _temp = line.split(",")  # List of csv line elements
+                for i, item in enumerate(_temp, 0):
+
+                    if not item.strip().isdigit():
+                        line_to_add += '\t"' + \
+                            self._header[i] + '":"' + item + '",\n'
+                    elif not item:
+                        line_to_add += '\t"' + self._header[i] + '":" ",\n'
+                    else:
+                        line_to_add += '\t"' + \
+                            self._header[i] + '":' + item + ",\n"
+                line_to_add += "\t},\n"
+                self.list_of_strings_to_add.append(line_to_add)
+            return self.list_of_strings_to_add
 
     # use RE here
+
     def parse(self):
         try:
-            with open(self.jsonfile, 'x') as f:
-                self.jsonstring += '{\n'
+            with FileManager(self.jsonfile, 'x') as f:
+                self.jsonstring += '[\n'
                 for line in self._get_values():
                     self.jsonstring += "\t" + line
                 self.jsonstring.rstrip(",\n")
-                self.jsonstring += "}\n"
+                self.jsonstring += "]\n"
                 f.write(self.jsonstring)
                 return True
         except FileExistsError:
@@ -78,4 +95,3 @@ if __name__ == "__main__":
     k = CsvToJson()
     k._get_header()
     k.parse()
-    
